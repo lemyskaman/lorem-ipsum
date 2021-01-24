4258d1a14f7b3a8490ff638ca36e74d301ff4fa5
lemys lopez <lemyskaman@gmail.com>
2021-01-24 17:54:46 -0400

    package boilerplate

---
+++ src/Providers/LorempIpsumServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lemyskaman\LorempIpsum;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LorempIpsumServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
